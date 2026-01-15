9da2504690f01afcac495470fd6649da59611f74
Adrian G <adrianguerrero0903@gmail.com>
2026-01-15 13:21:53 +0100

    Ajout 2 tests unitaire

--- dev/src/test/java/Tests.java
+++ dev/src/test/java/Tests.java
@@ -77,4 +77,52 @@ public class Tests {
 
         assertTrue(celluleTest.estVivante(), "Une cellule morte avec 3 voisins devrait naître");
     }
+
+    @Test // Vivante avec 2 voisines : survie
+    public void cellShouldStayAliveIfTwoNeighbors() {
+        Grille grille = new Grille(3,3);
+
+        // Grille test, 2 voisins
+        grille.setCellule(0,0, true);
+        grille.setCellule(1,0, true);
+        grille.setCellule(2,0, false);
+
+        grille.setCellule(0,1, false);
+        grille.setCellule(1,1, true);
+        grille.setCellule(2,1, false);
+
+        grille.setCellule(0,2, false);
+        grille.setCellule(1,2, false);
+        grille.setCellule(2,2, false);
+
+        grille = grille.prochaineEtape();
+
+        Cellule celluleTest = grille.getCellule(1, 1);
+
+        assertTrue(celluleTest.estVivante(), "Une cellule vivante avec 2 voisins devrait survivre");
+    }
+
+    @Test // Vivante avec 3 voisines : survie
+    public void cellShouldStayAliveIfThreeNeighbors() {
+        Grille grille = new Grille(3,3);
+
+        // Grille test, 3 voisins
+        grille.setCellule(0,0, true);
+        grille.setCellule(1,0, true);
+        grille.setCellule(2,0, true);
+
+        grille.setCellule(0,1, false);
+        grille.setCellule(1,1, true);
+        grille.setCellule(2,1, false);
+
+        grille.setCellule(0,2, false);
+        grille.setCellule(1,2, false);
+        grille.setCellule(2,2, false);
+
+        grille = grille.prochaineEtape();
+
+        Cellule celluleTest = grille.getCellule(1, 1);
+
+        assertTrue(celluleTest.estVivante(), "Une cellule vivante avec 3 voisins devrait survivre");
+    }
 }
